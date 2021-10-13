af3d2181f6fe6d5de01694e6488e9ca6cb116a5c
Gigio <90473003+giovannaBrigo@users.noreply.github.com>
2021-10-13 10:50:25 -0300

    Update ControladorDeLetrasJaDigitadas.java

--- ControladorDeLetrasJaDigitadas.java
+++ ControladorDeLetrasJaDigitadas.java
@@ -9,9 +9,8 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
     public ControladorDeLetrasJaDigitadas ()
     {
         // torna this.letrasJaDigitadas igual ao String vazio
-	//dany
-        
-        String letrasJaDigitadas = "";    //??????????
+	
+        String this.letrasJaDigitadas = "";   
     }
 
     public boolean isJaDigitada (char letra)
@@ -19,28 +18,48 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
         // percorrer o String this.letrasJaDigitadas e verificar se ele
         // possui a letra fornecida, retornando true em caso afirmativo
         // ou false em caso negativo
+
+       for(int i = 0; i < this.letrasJaDigitadas.length; i++)
+       {
+           if(letrasJaDigitadas.substring(i,i++) == letra)
+           {
+               return true;
+           }
+       }
+       return false;
     }
 
     public void registre (char letra) throws Exception
     {
-		// verifica se a letra fornecida ja foi digitada (pode usar
-		// o método this.isJaDigitada, para isso), lancando uma exceção
-		// em caso afirmativo.
-		// concatena a letra fornecida a this.letrasJaDigitadas.
-    }
+        // verifica se a letra fornecida ja foi digitada (pode usar
+        // o método this.isJaDigitada, para isso), lancando uma exceção
+        // em caso afirmativo.
+        // concatena a letra fornecida a this.letrasJaDigitadas.
 
+        if (this.isJaDigitada(letra))
+        throw new Exception ("Letra já digitada!");
+
+        this.letrasJaDigitadas+=letra;
+    }
+    
     @Override
     public String toString ()
     {
-		// retorna um String com TODAS as letras presentes em
-		// this.letrasJaDigitadas separadas por vírgula (,).
+        // retorna um String com TODAS as letras presentes em
+        // this.letrasJaDigitadas separadas por vírgula (,).
+
+        String strTodas = " ";
+
+        for(int i = 0; i < this.letrasJaDigitadas.length; i++)
+            strTodas += this.letrasJaDigitadas.substring(i,i++) + ", "; 
+
+        return strTodas;
     }
 
     @Override
     public boolean equals (Object obj)
     {
 	// verificar se this e obj são iguais
-        // dany
 	
 	if (this==obj) return true;
    
@@ -60,11 +79,10 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
     public int hashCode ()
     {
         // calcular e retornar o hashcode de this
-	//--> dany
     
-        int ret = super.hashcode();         // herda???
+        int ret = 5;     
         
-        ret = 7 * ret + new Byte (this.letrasJaDigitadas).hashCode();
+        ret = 7 * ret + new String(this.letrasJaDigitadas).hashCode();
         
         if(ret<0) ret = -ret // se for negativo, transformamos em positivo
         return ret;
@@ -75,7 +93,6 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
     throws Exception // construtor de cópia
     {
         // copiar c.letrasJaDigitadas em this.
-	//dany
     
         if(controladorDeLetrasJaDigitadas == null)
         throw new Exception("Modelo Ausente");
@@ -86,12 +103,17 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
     public Object clone ()
     {
         // criar uma cópia do this com o construtor de cópia e retornar
-	//dany
-
-        ControladorDeLetrasJaDigitadas  copia = new ControladorDeLetrasJaDigitadas (letrasJaDigitadas);
-                  //ou
-        ControladorDeLetrasJaDigitadas copia = (ControladorDeLetrasJaDigitadas)letrasJaDigitadas.clone();
-       
+	
+	Object copia=null;
+        
+        try
+        {
+		copia = new ControladorDeLetrasJaDigitadas(this); // object ou char ou tracinhos?
+		}
+		catch (Exception erro) // ignorando exception porque sabemos que não vai ocorrer, já que passamos this como parâmetro do construtor 
+		                         // de cópia e this é o objeto chamante do método clone
+		{}
+        
         return copia;
     }
 }
