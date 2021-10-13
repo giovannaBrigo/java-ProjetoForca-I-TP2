b88245aa52a15b57e6975aea6d20b3e949f39e16
FlausinoJulia <90473387+FlausinoJulia@users.noreply.github.com>
2021-10-12 21:20:05 -0300

    Update palavra.java

--- palavra.java
+++ palavra.java
@@ -66,11 +66,13 @@ public class Palavra implements Comparable<Palavra>
         return this.texto.length();
     }
 
+    @Override
     public String toString ()
     {
         return this.texto;
     }
 
+    @Override
     public boolean equals (Object obj) // ????????????????????????????
     {
         // verificar se this e obj possuem o mesmo conteúdo, retornando
@@ -90,6 +92,7 @@ public class Palavra implements Comparable<Palavra>
 		return true;			
     }
 
+    @Override
     public int hashCode () // ?????????????????????????????????????????
     {
         // calcular e retornar o hashcode de this
@@ -106,6 +109,7 @@ public class Palavra implements Comparable<Palavra>
         return ret;
     }
 
+    @Override
     public int compareTo (Palavra palavra)
     {
         return this.texto.compareTo(palavra.texto);
