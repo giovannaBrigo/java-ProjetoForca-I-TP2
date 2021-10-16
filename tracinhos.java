eac1cc9cd714c909a408281edbd922512db7a38a
FlausinoJulia <90473387+FlausinoJulia@users.noreply.github.com>
2021-10-16 17:29:10 -0300

    Update tracinhos.java

--- tracinhos.java
+++ tracinhos.java
@@ -12,11 +12,11 @@ public class Tracinhos implements Cloneable
 		// instancia this.texto com um vetor com tamanho igual qtd.
 		// preenche this.texto com underlines (_).
 		
-		if(qtd <= 0)
+		if (qtd <= 0)
 		    throw new Exception ("Quantidade de tracinhos inválida!");
 		    
 		this.texto = new char[qtd];
-		
+		 
 		for (int i = 0; i < qtd; i++)
 			this.texto[i] = '_';
     }
@@ -31,7 +31,7 @@ public class Tracinhos implements Cloneable
 		if ((posicao < 0) || (posicao >= this.texto.length))
 		    throw new Exception ("Posição inválida!");
 		    
-	    this.texto[posicao] = Character.toUpperCase(letra);
+	    this.texto[posicao] = letra;
     }
 
     public boolean isAindaComTracinhos ()
@@ -42,7 +42,7 @@ public class Tracinhos implements Cloneable
         // caso ainda reste algum underline, ou false caso
         // contrario
         
-        for (int i = 2; i < this.texto.length - 1; i++)
+        for (int i = 0; i < this.texto.length; i++)
         {
 			if (this.texto[i] == '_')
 				return true;
@@ -60,8 +60,8 @@ public class Tracinhos implements Cloneable
         
         String strTodos = "";
         
-        for(int i = 0; i < this.texto.length; i++)
-			strTodos += this.texto[i] + " ";
+        for(int i = 1; i < this.texto.length; i++)
+			strTodos += this.texto[1] + " ";
         
         return strTodos;
     }
@@ -80,14 +80,16 @@ public class Tracinhos implements Cloneable
 
         Tracinhos tracinhos = (Tracinhos)obj;
 
+		if (this.texto.length != tracinhos.texto.length) return false;
+
 		for(int i = 0; i < this.texto.length; i++)
 		{
-			if (tracinhos.texto[i] != this.texto[i])
-			return false;
+			if (tracinhos.texto[i] != (this.texto[i]))
+				return false;
 		}
         return true;
     }
-    
+
     @Override
     public int hashCode ()
     {
@@ -95,7 +97,7 @@ public class Tracinhos implements Cloneable
         
         int ret = 5;
         
-		ret = 5*ret + new String(this.texto).hashCode(); 
+		ret = 5*ret + this.texto.hashCode(); 
         
         if(ret < 0) ret = - ret;
         
@@ -129,7 +131,9 @@ public class Tracinhos implements Cloneable
 			copia = new Tracinhos(this);
 		}
 		catch (Exception erro)
-		{}
+		{} // ignoramos Exception porque sabemos que não ocorrerá
+		   // já que passamos this como parâmetro do construtor de cópia
+		   // e this é o objeto chamante do método clone, logo, não é nulo
         
         return copia;
     }
